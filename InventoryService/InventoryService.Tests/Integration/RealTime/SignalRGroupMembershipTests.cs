bf62b20cee40b1bb6eb8d81b390887288f37001d
Vlad <vladyslav.stepanko@gmail.com>
2026-09-22 20:00:14 +0300

    Revert "Fixed CI failure in SignalRGroupMembershipTests/FullPipelineE2ETests"
    This reverts commit d71340ad60b265826202a84352763d33c9fd5001.

--- InventoryService/InventoryService.Tests/Integration/RealTime/SignalRGroupMembershipTests.cs
+++ InventoryService/InventoryService.Tests/Integration/RealTime/SignalRGroupMembershipTests.cs
@@ -48,7 +48,7 @@ namespace InventoryService.Tests.Integration.RealTime
             });
 
             await connection.StartAsync();
-            await connection.InvokeWithRetryAsync("JoinWarehouse", joinedWarehouseId);
+            await connection.InvokeAsync("JoinWarehouse", joinedWarehouseId);
 
             await PublishStockLevelChangedAsync(otherWarehouseId);
 
